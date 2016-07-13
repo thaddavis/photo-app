132ae3f04293882c324fe51b467f1a923ae4f19c
thaddavis <thaddadavis@gmail.com>
2016-07-13 00:23:52 -0400

    Add Stripe Functionality

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0'
+# Stripe Gem for payment processing
+gem 'stripe'
 # devise authentication
 gem 'devise'
 # for bootstrap styling
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
+
+  has_one :payment
+  accepts_nested_attributes_for :payment
 end
